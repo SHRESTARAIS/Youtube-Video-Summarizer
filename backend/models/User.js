e05906932cc4ad0c3cb21f6bb067561f800d982a
SHRESTARAIS <raisshresta@gmail.com>
2025-11-09 21:53:58 +0530

    Fix: add default export to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,11 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-}, { timestamps: true });
-
-// Hash password before saving
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-// Compare password
-userSchema.methods.comparePassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
-};
+const User = mongoose.model("User", userSchema);
 
-module.exports = mongoose.model("User", userSchema);
+export default User;
